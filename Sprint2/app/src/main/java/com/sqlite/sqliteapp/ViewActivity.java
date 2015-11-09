a404aed355d2c36cb9fedb41c39e4c18aef65f2e
indishilpa <shilpa92@ufl.edu>
2015-11-09 17:35:21 -0500

    added rating functionality by other users

--- Sprint2/app/src/main/java/com/sqlite/sqliteapp/ViewActivity.java
+++ Sprint2/app/src/main/java/com/sqlite/sqliteapp/ViewActivity.java
@@ -5,7 +5,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
@@ -13,14 +17,21 @@ import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import java.util.List;
 
-/**
- * Created by Shilpa on 11/7/2015.
- */
 public class ViewActivity extends Activity{
+    private RatingBar ratingBar;
+    private TextView ratingValue;
+    private Button button;
+    private ParseUser rateThisUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_view);
+
+        addListenerOnRatingBar();
+        addListenerOnButton();
+
 
         Intent intent = getIntent();
         final String userName = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
@@ -32,15 +43,48 @@ public class ViewActivity extends Activity{
         query.findInBackground(new FindCallback<ParseUser>() {
             public void done(List<ParseUser> objects, ParseException e) {
                 if (e == null) {
-                    for (int i = 0; i < objects.size(); i++) {
-                        ParseUser u = objects.get(i);
-                        String add = u.getString("Address");
+                    //for (int i = 0; i < objects.size(); i++) {
+                        rateThisUser = objects.get(0);
+                        String add = rateThisUser.getString("Address");
                         textView.setText(userName + "\t" + add);
-                        Log.d("tag",userName + "\t" + add);
-                    }
+                        Log.d("tag", userName + "\t" + add);
+                    //}
                 }
             }
         });
+    }
+
+    public void addListenerOnRatingBar() {
+
+        ratingBar = (RatingBar) findViewById(R.id.ratingBar);
+        ratingValue = (TextView) findViewById(R.id.txtRatingValue);
+
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+
+                ratingValue.setText(String.valueOf(rating));
+                rateThisUser.put("Rating", String.valueOf(rating));
+                rateThisUser.saveEventually();
+
+            }
+        });
+    }
+
+    public void addListenerOnButton() {
+
+        ratingBar = (RatingBar) findViewById(R.id.ratingBar);
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ViewActivity.this, String.valueOf(ratingBar.getRating()), Toast.LENGTH_LONG).show();
+            }
+
+        });
+
     }
 
     @Override
